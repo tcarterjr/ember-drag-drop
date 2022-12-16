3162bcdb7519e81f3e8bdb7c20e84d7ce78db68e
Tom Carter <tommyjr@gmail.com>
2022-12-16 16:54:34 +0000

    Updated ember-try scenarios

--- config/ember-try.js
+++ config/ember-try.js
@@ -8,14 +8,6 @@ module.exports = async function () {
     useYarn: true,
     scenarios: [
       {
-        name: 'ember-lts-3.24',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.24.3',
-          },
-        },
-      },
-      {
         name: 'ember-lts-3.28',
         npm: {
           devDependencies: {
@@ -24,10 +16,18 @@ module.exports = async function () {
         },
       },
       {
-        name: 'ember-lts-3.24',
+        name: 'ember-lts-4.4',
         npm: {
           devDependencies: {
-            'ember-source': '~3.24.0',
+            'ember-source': '~4.4.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-4.8',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.8.0',
           },
         },
       },
